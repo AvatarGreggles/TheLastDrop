2bfa39be9cd1749e1da45d7c5d649ee2db63d741
Greggles <gregory@wetransfer.com>
2022-08-05 15:44:16 +0200

    Player basic movement and base scene setup

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        // Move sideways
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
+
+        // Handle direction change
+        if (theRB.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (theRB.velocity.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
     }
 }
