3e6a2a3d5f8db48e07c6fdb8a139a584558b3bc1
Greggles <gregory@wetransfer.com>
2022-08-05 15:59:24 +0200

    Implement jumping logic

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,6 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D theRB;
     public float moveSpeed;
+    public float jumpForce;
+    public Transform groundCheckPoint;
+    private bool isOnGround;
+    public LayerMask whatIsGround;
+    private bool canDoubleJump;
+    private float jumpBufferTime = 0.2f;
+    float jumpBufferCounter = 0f;
+    private float coyoteTime = 1f;
+    float coyoteTimeCounter = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +25,32 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
         HandleMovement();
+
+        // Checking if on the ground
+        isOnGround = Physics2D.OverlapCircle(groundCheckPoint.position, .3f, whatIsGround);
+
+        if (isOnGround)
+        {
+            Debug.Log("OnGround");
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            Debug.Log("Not on ground");
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        HandleJumping();
     }
 
     void HandleMovement()
@@ -32,6 +66,32 @@ public class PlayerMovement : MonoBehaviour
         else if (theRB.velocity.x > 0)
         {
             transform.localScale = Vector3.one;
+        }
+    }
+
+    void HandleJumping()
+    {
+        if (coyoteTimeCounter > 0 && jumpBufferCounter > 0)
+        {
+            jumpBufferCounter = 0f;
+
+            if (isOnGround)
+            {
+                canDoubleJump = true;
+            }
+            else
+            {
+                canDoubleJump = false;
+            }
+
+            // Jumping
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+
+        if (Input.GetButtonUp("Jump") && theRB.velocity.y > 0f)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, theRB.velocity.y * 0.5f);
+            coyoteTimeCounter = 0f;
         }
     }
 }
