68c2d03d5451826ea97d87a5be25a8bbf76ec33e
Greggles <gregory@wetransfer.com>
2022-08-05 21:24:37 +0200

    Fill sprite based on amount of water

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager instance;
     public PlayerMovement playerMovement;
+
+    [SerializeField] public Image playerWaterBodySprite;
+    float maxWaterLevel = 1f;
+    float currentWaterLevel;
 
     private void Awake()
     {
@@ -15,5 +20,50 @@ public class PlayerManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    void Start()
+    {
+        currentWaterLevel = maxWaterLevel;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            GainWater();
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            LoseWater();
+        }
+    }
+
+    void GainWater()
+    {
+        currentWaterLevel += 0.01f;
+        if (currentWaterLevel > maxWaterLevel)
+        {
+            currentWaterLevel = maxWaterLevel;
+        }
+
+        UpdatePlayerWaterBodySprite();
+    }
+
+    void LoseWater()
+    {
+        currentWaterLevel -= 0.01f;
+        if (currentWaterLevel < 0)
+        {
+            currentWaterLevel = 0;
+        }
+
+        UpdatePlayerWaterBodySprite();
+    }
+
+    void UpdatePlayerWaterBodySprite()
+    {
+        playerWaterBodySprite.fillAmount = currentWaterLevel;
     }
 }
